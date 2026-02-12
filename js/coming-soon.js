e1e7e421588a2be476db5ff93f9260389229fcdd
Andrés <andresgarcia@tretto.co>
2026-02-12 10:15:34 -0500

    Coming Soon

---
+++ js/coming-soon.js
@@ -0,0 +1,9 @@
+console.log("Coming Soon — Cultura Profética");
+
+window.addEventListener("load", () => {
+  const title = document.querySelector(".title");
+  const social = document.querySelector(".social");
+
+  title.classList.add("fade-in");
+  social.classList.add("fade-in-delay");
+});
